7671c271e857d08954613cb983680965d0816973
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-17 20:58:55 -0300

    start setup + deploy db

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import { db } from "../database/database.js"
+
+export async function authValidation(req,res,next){
+    const { authorization } = req.headers
+
+    const token = authorization?.replace('Bearer ', '')
+    if (!token) res.sendStatus(401)
+    
+    try{
+        // ******************************** alterar **************************
+        // const sessao = await db.collection("sessoes").findOne({ token })
+        // if (!sessao) return res.sendStatus(401) 
+    
+        res.locals.sessao=sessao
+    }catch(err){
+        return res.status(500).send(err.message)
+    }
+    
+    next()
+}
