391b5f0d9e59be61b8e40a77e345ed1ff3d91300
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-18 18:12:23 -0300

    fix post /url/shorten

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -10,7 +10,7 @@ export async function postShorten(req,res){
         await db.query(`insert into urls ("shortUrl",url) values($1,$2);`,[random,url])
         const result=await db.query(`select * from urls where url=$1;`,[url])
         delete result.rows[0].url
-        res.status(201).send(result)
+        res.status(201).send(result.rows[0])
     }catch(err){
         res.status(500).send(err.message)
     }
