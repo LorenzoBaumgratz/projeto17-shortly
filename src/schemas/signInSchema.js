c42f4bc33cfa2f9deb938114d6ab098ce6914995
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-17 23:14:25 -0300

    fix db2

--- src/schemas/signInSchema.js
+++ src/schemas/signInSchema.js
@@ -1,6 +1,6 @@
 import Joi from "joi";
 
 export const signInSchema=Joi.object({
-    email:Joi.email().required(),
+    email:Joi.string().email().required(),
     password:Joi.string().min(1).required()
 })
--- src/schemas/signUpSchema.js
+++ src/schemas/signUpSchema.js
@@ -2,7 +2,7 @@ import Joi from "joi";
 
 export const signUpSchema=Joi.object({
     name:Joi.string().min(1).required(),
-    email:Joi.email().required(),
+    email:Joi.string().email().required(),
     password:Joi.string().min(1).required(),
     confirmPassword:Joi.string().min(1).required()
 })
