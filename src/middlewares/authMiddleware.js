661074303d8bb1d06e478d8c8486c05f5f559a41
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-18 21:44:37 -0300

    hub test

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -10,7 +10,6 @@ export async function authValidation(req,res,next){
     try{
         const sessao = await db.query(`select * from sessions where token=$1;`,[token])
         if (sessao.rowCount===0) return res.sendStatus(401) 
-        console.log(await db.query(`select * from sessions where token=$1;`,[token]))
         res.locals.sessao=sessao
     }catch(err){
         return res.status(500).send(err.message)
