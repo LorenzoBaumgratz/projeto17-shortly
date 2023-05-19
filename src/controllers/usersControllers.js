34bc39883d63afca2daf7b2ff07d6be50982b582
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-19 20:54:05 -0300

    fix getMe 5

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -5,8 +5,18 @@ export async function getMe(req,res){
         const sessionId=res.locals.sessao.rows[0].id
 
         const result=await db.query(`select urls.*,urls.id as "idDaUrl",users.name,users.id as "idDoUsuario",sessions.id from urls join sessions on urls."sessionId"=sessions.id join users on sessions."userId"=users.id where sessions.id=$1;`,[sessionId])
-        console.log(result.rows)
-
+        const obj={
+            id:result.rows[0].idDoUsuario,
+            name:result.rows[0].name,
+            visitCount:"total",
+            shortenedUrls:result.rows.map(i=>({
+                id: i.idDaUrl,
+                shortUrl:i.shortUrl,
+                url:i.url,
+                visitCount:i.visitCount
+            }))
+        }
+        console.log(obj)
         res.sendStatus(200)
     }catch(err){
         res.status(500).send(err.message)
