b2b1fd71d530cfcd67801594d6b85ecf42616b0f
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-18 23:12:55 -0300

    feat getUrlById

--- src/controllers/urlsControllers.js
+++ src/controllers/urlsControllers.js
@@ -9,10 +9,28 @@ export async function postShorten(req,res){
         const random=nanoid()
         await db.query(`insert into urls ("shortUrl",url) values($1,$2);`,[random,url])
         const result=await db.query(`select * from urls where url=$1;`,[url])
+
         delete result.rows[0].url
         delete result.rows[0].visitCount
         delete result.rows[0].createdAt
+
         res.status(201).send(result.rows[0])
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+}
+
+export async function getUrlById(req,res){
+    const {id}=req.params
+
+    try{
+        const result=await db.query(`select * from urls where id=$1;`,[id])
+        if(result.rowCount===0) return res.sendStatus(404)
+
+        delete result.rows[0].visitCount
+        delete result.rows[0].createdAt
+
+        res.status(200).send(result.rows[0])
     }catch(err){
         res.status(500).send(err.message)
     }
--- src/routes/urlsRouter.js
+++ src/routes/urlsRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { authValidation } from "../middlewares/authMiddleware.js";
-import { postShorten } from "../controllers/urlsControllers.js";
+import { getUrlById, postShorten } from "../controllers/urlsControllers.js";
 import { validateSchema } from "../middlewares/validateSchemaMiddleware.js";
 import { postShortenSchema } from "../schemas/postShortenSchema.js";
 
 const urls=Router()
 
 urls.post("/urls/shorten",authValidation,validateSchema(postShortenSchema),postShorten)
+urls.get("/urls/:id",getUrlById)
 
 export default urls
