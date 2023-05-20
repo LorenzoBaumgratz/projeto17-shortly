2aa9575a5bc64ac2f47efd6a2ffd85965da91a6f
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-19 22:41:26 -0300

    fix ranking3.1

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,36 +1,36 @@
 import { db } from "../db/database.js"
 
-export async function getMe(req,res){
-    try{
-        const sessionId=res.locals.sessao.rows[0].id
+export async function getMe(req, res) {
+    try {
+        const sessionId = res.locals.sessao.rows[0].id
 
-        const result=await db.query(`select urls.*,urls.id as "idDaUrl",users.name,users.id as "idDoUsuario",sessions.id from urls join sessions on urls."sessionId"=sessions.id join users on sessions."userId"=users.id where sessions.id=$1;`,[sessionId])
-        const sum=await db.query(`select sum("visitCount") as soma from urls where "sessionId"=$1;`,[sessionId])
-        const obj={
-            id:result.rows[0].idDoUsuario,
-            name:result.rows[0].name,
-            visitCount:Number(sum.rows[0].soma),
-            shortenedUrls:result.rows.map(i=>({
+        const result = await db.query(`select urls.*,urls.id as "idDaUrl",users.name,users.id as "idDoUsuario",sessions.id from urls join sessions on urls."sessionId"=sessions.id join users on sessions."userId"=users.id where sessions.id=$1;`, [sessionId])
+        const sum = await db.query(`select sum("visitCount") as soma from urls where "sessionId"=$1;`, [sessionId])
+        const obj = {
+            id: result.rows[0].idDoUsuario,
+            name: result.rows[0].name,
+            visitCount: Number(sum.rows[0].soma),
+            shortenedUrls: result.rows.map(i => ({
                 id: i.idDaUrl,
-                shortUrl:i.shortUrl,
-                url:i.url,
-                visitCount:i.visitCount
+                shortUrl: i.shortUrl,
+                url: i.url,
+                visitCount: i.visitCount
             }))
         }
         res.status(200).send(obj)
-    }catch(err){
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
 
-export async function ranking(req,res){
-    try{
-        const result=await db.query(`select users.id,users.name,session.id,count(*) as "linksCount", sum("visitCount") as total
+export async function ranking(req, res) {
+    try {
+        const result = await db.query(`select users.id,users.name,sessions.id,count(*) as "linksCount", sum("visitCount") as total
         from urls left join sessions on urls."sessionId"=sessions.id 
         left join users on sessions."userId"=users.id group by users.id,users.name,"linksCount" order by total;`)
         console.log(result.rows)
         res.sendStatus(200)
-    }catch(err){
+    } catch (err) {
         res.status(500).send(err.message)
     }
 }
