95c587232a407678cb83a31d0d3e92368b35607e
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-19 21:12:11 -0300

    fix getMe ???

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -5,12 +5,12 @@ export async function getMe(req,res){
         const sessionId=res.locals.sessao.rows[0].id
 
         const result=await db.query(`select urls.*,urls.id as "idDaUrl",users.name,users.id as "idDoUsuario",sessions.id from urls join sessions on urls."sessionId"=sessions.id join users on sessions."userId"=users.id where sessions.id=$1;`,[sessionId])
-        const sum=await db.query(`select sum("visitCount") from urls where "sessionId"=$1;`,[sessionId])
-        console.log("SOMA",sum.rows)
+        const sum=await db.query(`select sum("visitCount") as soma from urls where "sessionId"=$1;`,[sessionId])
+        console.log("SOMA",sum.rows[0].soma)
         const obj={
             id:result.rows[0].idDoUsuario,
             name:result.rows[0].name,
-            visitCount:sum.rows[0],
+            visitCount:sum.rows[0][0],
             shortenedUrls:result.rows.map(i=>({
                 id: i.idDaUrl,
                 shortUrl:i.shortUrl,
