aec2a025bb9a942f59487c3eda0996ee50751264
LorenzoBaumgratz <lorenzobaumgratz@yahoo.com.br>
2023-05-19 22:00:09 -0300

    feat ranking

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -23,3 +23,12 @@ export async function getMe(req,res){
         res.status(500).send(err.message)
     }
 }
+
+export async function ranking(req,res){
+    try{
+        console.log(await db.query(`select urls.*,urls.id as "idDaUrl",users.name,users.id as "idDoUsuario",sessions.id from urls join sessions on urls."sessionId"=sessions.id join users on sessions."userId"=users.id group by "userId";`))
+        res.sendStatus(200)
+    }catch(err){
+        res.status(500).send(err.message)
+    }
+}
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
 import { authValidation } from "../middlewares/authMiddleware.js";
-import { getMe } from "../controllers/usersControllers.js";
+import { getMe, ranking } from "../controllers/usersControllers.js";
 
 const users=Router()
 
 users.get("/users/me",authValidation,getMe)
-
+users.get("/ranking",ranking)
 export default users
